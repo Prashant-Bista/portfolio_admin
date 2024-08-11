53b5ebdb1144c8e3efa274ef6df3013e964b9a76
Prashant <bistaprashant1@gmail.com>
2024-08-11 21:24:56 +0545

    fetched Data from firebase

--- lib/read_messages.dart
+++ lib/read_messages.dart
@@ -6,10 +6,11 @@ import 'package:portfolio_admin/components.dart';
 
 
 
-List lst= [];
 List messages=[];
-List Contacts = [];
-List firstnames= [];
+List contacts = [];
+List firstNames= [];
+List lastNames=[];
+List emails=[];
 
 var logger = Logger();
 
@@ -24,7 +25,27 @@ class _ReadMessagesState extends State<ReadMessages> {
   Future getMessages() async{
     await FirebaseFirestore.instance.collection("messages").get().then((value){
       value.docs.forEach((element){
-        lst.add(element.data()['message']);
+        messages.add(element.data()['message']);
+      });
+    });
+    await FirebaseFirestore.instance.collection("messages").get().then((value){
+      value.docs.forEach((element){
+        firstNames.add(element.data()['first name']);
+      });
+    });
+    await FirebaseFirestore.instance.collection("messages").get().then((value){
+      value.docs.forEach((element){
+        lastNames.add(element.data()['last name']);
+      });
+    });
+    await FirebaseFirestore.instance.collection("messages").get().then((value){
+      value.docs.forEach((element){
+        emails.add(element.data()['email']);
+      });
+    });
+    await FirebaseFirestore.instance.collection("messages").get().then((value){
+      value.docs.forEach((element){
+        contacts.add(element.data()['phone number']);
       });
     });
 
@@ -32,6 +53,7 @@ class _ReadMessagesState extends State<ReadMessages> {
   @override
   void initState() {
     getMessages();
+    print(messages.length);
     // TODO: implement initState
     super.initState();
   }
@@ -75,8 +97,9 @@ class _ReadMessagesState extends State<ReadMessages> {
             )
           ];
         },
-body: ListView.builder(itemCount: lst.length,itemBuilder: (context, int index){
-
+body: ListView.builder(itemCount: messages.length,itemBuilder: (context, int index){
+  return Messages(index: index,firstname: firstNames[index], email: emails[index], message: messages[index],lastname: lastNames[index],contact: contacts[index],);
+  print(index);
 }),        ),
       );
   }
@@ -88,6 +111,7 @@ class Messages extends StatelessWidget {
   final email;
   final contact;
   final message;
+  final index;
 
   const Messages(
       {super.key,
@@ -95,10 +119,11 @@ class Messages extends StatelessWidget {
       this.lastname,
       @required this.email,
       this.contact,
-      @required this.message});
+      @required this.message, this.index});
 
   @override
   Widget build(BuildContext context) {
+    double widthDevice= MediaQuery.of(context).size.width;
     return             Wrap(children: [
       Container(
         decoration: BoxDecoration(
@@ -111,7 +136,7 @@ class Messages extends StatelessWidget {
             SizedBox(width: 5,),
             RobotoText(
               size: 20,
-              text: "1)",
+              text: "${index+1}",
               color: Colors.white,
             ),
             MessageHeading(label: "FirstName", data: firstname ,),
@@ -122,9 +147,10 @@ class Messages extends StatelessWidget {
         ),
       ),
       Container(
+        width: widthDevice,
         padding: EdgeInsets.all(20),
         color:Colors.white,
-        child: RobotoText(text: "Message:\n dfslkfja;ljfa;lskjf;lakjsdf;asklfdjsa;ldfkjs;ldkfjdfal;jf;laksjfd;laksjf;laksjfd;lkjfdsa;lkfj",size: 20,),
+        child: RobotoText(text: "Message:\n $message",size: 20,),
       ),
     ],);
 
