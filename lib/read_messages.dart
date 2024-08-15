d4e79e6872a97667f63f1776806424dff7a29418
Prashant <bistaprashant1@gmail.com>
2024-08-15 21:19:40 +0545

    changed message UI

--- lib/read_messages.dart
+++ lib/read_messages.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:portfolio_admin/components.dart';
@@ -12,19 +13,21 @@ class ReadMessages extends StatefulWidget {
 
 class _ReadMessagesState extends State<ReadMessages> {
   bool _isLoading = true;
+  List Ids=[];
   List messages = [];
   List contacts = [];
   List firstNames = [];
   List lastNames = [];
   List emails = [];
-  var snapShots;
+  var snapshot;
   var logger = Logger();
   Future getMessages() async {
+    var doc = FirebaseFirestore.instance.collection("messages").doc().id;
+    print(doc);
     try {
-      var snapshot =
+      snapshot =
       await FirebaseFirestore.instance.collection("messages").get();
       setState(() {
-
         for (var docs in snapshot.docs) {
           firstNames.add(docs.data()["first name"]);
           lastNames.add(docs.data()["last name"]);
@@ -39,7 +42,6 @@ class _ReadMessagesState extends State<ReadMessages> {
       logger.d("Failed to fetch data");
     }
   }
-
 
   @override
   void initState() {
@@ -71,6 +73,7 @@ class _ReadMessagesState extends State<ReadMessages> {
                     itemCount: messages.length,
                     itemBuilder: (context, index) {
                       return Messages(
+                        // ID:ids[index],
                         index: index,
                         firstname: firstNames[index],
                         email: emails[index],
@@ -84,6 +87,7 @@ class _ReadMessagesState extends State<ReadMessages> {
 }
 
 class Messages extends StatelessWidget {
+  final id;
   final firstname;
   final lastname;
   final email;
@@ -98,30 +102,46 @@ class Messages extends StatelessWidget {
       @required this.email,
       this.contact,
       @required this.message,
-      this.index});
+      this.index, this.id});
 
   @override
   Widget build(BuildContext context) {
     double widthDevice = MediaQuery.of(context).size.width;
-    return Wrap(
+    return Column(
       children: [
+        SizedBox(height: 50,),
         Container(
           decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10) ,),
               border: Border(
                   top: BorderSide(color: Colors.blue, width: 5.0),
                   left: BorderSide(color: Colors.blue, width: 5.0),
                   right: BorderSide(color: Colors.blue, width: 5.0)),
               color: Colors.grey,
               boxShadow: [BoxShadow(blurRadius: 5, color: Colors.grey)]),
-          child: Wrap(
+          child: Column(
             children: [
               SizedBox(
                 width: 5,
               ),
-              RobotoText(
-                size: 20,
-                text: "${index + 1}",
-                color: Colors.white,
+              Row(
+                children:[ RobotoText(
+                  size: 20,
+                  text: "${index + 1}",
+                  color: Colors.white,
+                ),
+                SizedBox(width: widthDevice/1.1,),
+                IconButton(padding:EdgeInsets.zero,onPressed: () async{
+                  // AlertDialog(
+                  //     title: RobotoText(text: "Do you really want to delete the message",size: 20,color: Colors.black,),
+                  //     icon: Icon(Icons.warning,color: Colors.yellow,),);
+                  var snapshot= await FirebaseFirestore.instance.collection("message").where("phone number", isEqualTo: contact).get();
+                  for (var doc in snapshot.docs){
+                    print(doc.id);
+                  }
+
+                }, icon: Icon(Icons.delete),color: Colors.red,),
+                ]
               ),
               MessageHeading(
                 label: "FirstName",
@@ -142,20 +162,43 @@ class Messages extends StatelessWidget {
             ],
           ),
         ),
+
         Container(
           width: widthDevice,
           padding: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
-          child: RobotoText(
-            text: "Message:\n $message",
-            size: 20,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding:EdgeInsets.all(5),
+                decoration:BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.lightBlueAccent,
+                ),
+                child: RobotoText(
+                  text: "Message:",
+                  size: 20,
+                  isbold: true,
+                  color: Colors.black,
+                ),
+              ),
+              RobotoText(
+                text: message,
+                size: 20,
+              )
+            ],
           ),
           decoration: BoxDecoration(
+            borderRadius:BorderRadius.only(bottomLeft:  Radius.circular(10),bottomRight:  Radius.circular(10)) ,
               border: Border(
                 left: BorderSide(color: Colors.blue, width: 5.0),
                 right: BorderSide(color: Colors.blue, width: 5.0),
+                bottom: BorderSide(color: Colors.blue, width: 5.0)
               ),
               color: Colors.white),
         ),
+        SizedBox(height: 50,)
       ],
     );
   }
